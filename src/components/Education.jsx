666e664c0763147ad8c48a59d3ac9f13d1a7d5fb
AshutoshVerma140404 <happy786wwe@gmail.com>
2025-04-27 19:22:59 +0530

    xyz

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -137,7 +137,6 @@ const Education = ({ languageType, setActiveScreen, activeScreen }) => {
         languageType={languageType}
       />
 
-      <Footer languageType={languageType} setActiveScreen={setActiveScreen} activeScreen={activeScreen} />
     </div>
   );
 };
